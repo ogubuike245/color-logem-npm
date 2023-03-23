972006868f70c28b4a3d9f85492bc299fc5f1b23
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-03-23 14:20:49 +0100

    publish package to npm

--- README.md
+++ README.md
@@ -1,9 +1,58 @@
-# ColorLogem-npm
+# color-logem-npm
 
-ColorLogem - A Lightweight and Customizable Colorful Console Logger
+color-logem - A Lightweight and Customizable Colorful Console Logger
 
 # Features
 
 - Customizable colors and styles for console output
 - Lightweight and easy to use
 - Supports logging to the console in a variety of colors and styles, including red, green, yellow, blue, magenta, and cyan
+
+# Installation
+
+You can install color-logem using npm:
+
+```bash
+npm install @buike/color-logem
+```
+
+# Usage
+
+```bash
+import { log , info, warning, error } = require("@buike/color-logem");
+
+# Log a blue message to the console with white background and bold text
+log("Hello, world!");
+
+# Log a blue message to the console with white background and bold text
+info("info: some information!");
+
+#  Log a yellow warning message to the console with white background and bold text
+warning("Warning: something went wrong!");
+
+#  Log a red error message to the console with white background and bold text
+error("Error: something went terribly wrong!");
+
+```
+
+# API
+
+`log(args: any)`
+Logs a message to the console in blue text with white background and bold text.
+
+`info(args: any)`
+Logs an information message to the console in blue text with white background and normal text.
+
+`warning(args: any)`
+Logs a warning message to the console in yellow text with white background and bold text.
+
+`error(args: any)`
+Logs an error message to the console in red text with white background and bold text.
+
+# License
+
+color-logem is released under the MIT License. See LICENSE file for details.
+
+# Contributing
+
+Contributions are welcome! Please feel free to submit a pull request or open an issue if you encounter any bugs or have any suggestions for improvement.
--- index.js
+++ index.js
@@ -1,48 +1,26 @@
-export default class Logger {
-    private static getColor(color: string) {
-        switch (color) {
-            case "red":
-                return "\x1b[31m"; // red
-            case "green":
-                return "\x1b[32m"; // green
-            case "yellow":
-                return "\x1b[33m"; // yellow
-            case "blue":
-                return "\x1b[34m"; // blue
-            case "magenta":
-                return "\x1b[35m"; // magenta
-            case "cyan":
-                return "\x1b[36m"; // cyan
-            default:
-                return "\x1b[0m"; // reset
-        }
-    }
+import chalk from "chalk";
 
-    private static format(args: any, color: string, bgColor: string, bold: boolean) {
-        const timestamp = new Date().toLocaleString();
-        const colorCode = this.getColor(color);
-        const bgColorCode = this.getColor(bgColor);
-        const boldCode = bold ? "\x1b[1m" : "";
-        const resetCode = "\x1b[0m";
+export const log = (args) => {
+  info(args);
+};
 
-        const message = typeof args === "string" ? args : JSON.stringify(args);
+export const info = (args) => {
+  console.log(
+    chalk.white.bgBlue.bold(`[${new Date().toLocaleString()}] [INFO]`),
+    typeof args === "string" ? chalk.blueBright(args) : args
+  );
+};
 
-        return `${colorCode}${bgColorCode}${boldCode}[${timestamp}] [${color.toUpperCase()}] ${message}${resetCode}`;
-    }
+export const warning = (args) => {
+  console.log(
+    chalk.white.bgYellow.bold(`[${new Date().toLocaleString()}] [WARN]`),
+    typeof args === "string" ? chalk.yellowBright(args) : args
+  );
+};
 
-    public static log(args: any) {
-        console.log(this.format(args, "blue", "white", true));
-    }
-
-    public static info(args: any) {
-        console.log(this.format(args, "blue", "white", false));
-    }
-
-    public static warning(args: any) {
-        console.log(this.format(args, "yellow", "white", true));
-    }
-
-    public static error(args: any) {
-        console.log(this.format(args, "red", "white", true));
-    }
-}
+export const error = (args) => {
+  console.log(
+    chalk.white.bgRed.bold(`[${new Date().toLocaleString()}] [ERROR]`),
+    typeof args === "string" ? chalk.redBright(args) : args
+  );
+};
